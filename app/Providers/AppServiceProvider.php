caac8c841b981302ef34b1e154dc56dcc7d29ed0
ACS-83 <souperform@gmail.com>
2022-12-12 21:41:11 +0100

    second commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Order;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // $userLogged = auth()->check();
+        // dd($userLogged);
+        // if(isset($userLogged) && Auth::user()->role == 1) {
+        //     Order::all()->where('sent', '=', '0')->count();
+        //     View::share('ordersChecking', $ordersChecking);
+        // }
+
+        // if(isset($userLogged) && Auth::user()->role == 0) {
+        //     $user = Auth::user()->email;
+        //     $ordersChecking = Order::all()->where('mailuser', '=', $user)->where('sent', '=', 0)->count();
+        // }
+
+
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- Nombre de logo (superior izquierda) --}}
@@ -27,8 +26,7 @@
         }
     </style>
     <!-- Scripts -->
-    {{-- @vite(['resources/js/app.js']) --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    @vite(['resources/js/app.js'])
 </head>
 
 <body>
@@ -37,20 +35,23 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name') }}
+                    
                 </a>
                 
-
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-             
+                    @if (isset($ordersChecking))
+                        {{-- {{ dd($ordersChecking) }} --}}
+                    @endif
                     <!-- Izquierda Navbar -->
-                    
                     <!-- Derecha Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <a class="nav-link mx-auto" href="{{ route('products.index') }}">{{ __('Productos') }}</a>
-                    
+                    {{-- @if (Auth::check())
+                        {{ dd($ordersChecking) }}
+                    @endif --}}
                         <a class="nav-link mx-auto" href="{{ route('us') }}">{{ __('Nosotros') }}</a>
                         <!-- Autentificación -->
                         {{-- Si el usuario está registrado, su rol es 0 (usuario) y está verificado... --}}
